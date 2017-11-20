aefe25a3f9b49837059e477d518ad247c604d821
enrrike19 <enrrike19@gmail.com>
2017-11-19 20:03:49 -0500

    primera subida

--- app/src/main/java/pe/empresab3/notialertas/presentacion/NetworkUtils.java
+++ app/src/main/java/pe/empresab3/notialertas/presentacion/NetworkUtils.java
@@ -3,6 +3,8 @@ package pe.empresab3.notialertas.presentacion;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.EventLogTags;
+import android.util.Log;
 
 /**
  * Created by Enrique on 18/11/2017.
@@ -10,12 +12,16 @@ import android.net.NetworkInfo;
 
 public class NetworkUtils {
 
+
+    private static final String TAG = "VALIDAR_CONEXION";
     public static boolean hayInternet(Context context) {
         boolean hayInternet;
         ConnectivityManager connectivityManager =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         hayInternet = (networkInfo != null && networkInfo.isConnectedOrConnecting());
+
+        Log.d(TAG, "hayInternet: " + hayInternet);
         return hayInternet;
     }
 }
