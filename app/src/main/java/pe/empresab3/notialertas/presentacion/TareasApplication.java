03db521a377f0765c7a13ccf1b80b46614c222bc
enrrike19 <enrrike19@gmail.com>
2017-11-23 22:48:40 -0500

    TareaApplication y TareaEntity

---
+++ app/src/main/java/pe/empresab3/notialertas/presentacion/TareasApplication.java
@@ -0,0 +1,27 @@
+package pe.empresab3.notialertas.presentacion;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+
+/**
+ * Created by Enrique on 23/11/2017.
+ */
+
+public class TareasApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+    }
+}
